6c28b86419f5e87526749fa975fc7d3ca79a5dc4
lewisxhe <lewishe@outlook.com>
2025-11-05 13:46:07 +0800

    Fixed lvgl 9.4 example[ no ci]

--- examples/lvgl/widgets/chart/lv_example_chart_5.c
+++ examples/lvgl/widgets/chart/lv_example_chart_5.c
@@ -5,13 +5,14 @@ static void hook_division_lines(lv_event_t * e);
 static void add_faded_area(lv_event_t * e);
 static void draw_event_cb(lv_event_t * e);
 
+
 /**
  * Add a faded area effect to the line chart and make some division lines ticker
  */
 void lv_example_chart_5(void)
 {
     /*Create a chart*/
-    lv_obj_t *chart = lv_chart_create(lv_screen_active());
+    lv_obj_t * chart = lv_chart_create(lv_screen_active());
     lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /*Show lines and points too*/
     lv_obj_set_size(chart, 200, 150);
     lv_obj_set_style_pad_all(chart, 0, 0);
@@ -23,43 +24,43 @@ void lv_example_chart_5(void)
     lv_obj_add_event_cb(chart, draw_event_cb, LV_EVENT_DRAW_TASK_ADDED, NULL);
     lv_obj_add_flag(chart, LV_OBJ_FLAG_SEND_DRAW_TASK_EVENTS);
 
-    lv_chart_series_t *ser = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
+    lv_chart_series_t * ser = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
 
     uint32_t i;
-    for (i = 0; i < 10; i++) {
-        lv_chart_set_next_value(chart, ser, lv_rand(10, 80));
+    for(i = 0; i < 10; i++) {
+        lv_chart_set_next_value(chart, ser, (int32_t)lv_rand(10, 80));
     }
 }
 
 static void draw_event_cb(lv_event_t * e)
 {
-    lv_draw_task_t *draw_task = lv_event_get_draw_task(e);
-    lv_draw_dsc_base_t *base_dsc = lv_draw_task_get_draw_dsc(draw_task);
+    lv_draw_task_t * draw_task = lv_event_get_draw_task(e);
+    lv_draw_dsc_base_t * base_dsc = (lv_draw_dsc_base_t *)lv_draw_task_get_draw_dsc(draw_task);
 
-    if (base_dsc->part == LV_PART_ITEMS && lv_draw_task_get_type(draw_task) == LV_DRAW_TASK_TYPE_LINE) {
+    if(base_dsc->part == LV_PART_ITEMS && lv_draw_task_get_type(draw_task) == LV_DRAW_TASK_TYPE_LINE) {
         add_faded_area(e);
 
     }
     /*Hook the division lines too*/
-    if (base_dsc->part == LV_PART_MAIN && lv_draw_task_get_type(draw_task) == LV_DRAW_TASK_TYPE_LINE) {
+    if(base_dsc->part == LV_PART_MAIN && lv_draw_task_get_type(draw_task) == LV_DRAW_TASK_TYPE_LINE) {
         hook_division_lines(e);
     }
 }
 
 static void add_faded_area(lv_event_t * e)
 {
-    lv_obj_t *obj = lv_event_get_target(e);
+    lv_obj_t * obj = lv_event_get_target_obj(e);
     lv_area_t coords;
     lv_obj_get_coords(obj, &coords);
 
-    lv_draw_task_t *draw_task = lv_event_get_draw_task(e);
-    lv_draw_dsc_base_t *base_dsc = lv_draw_task_get_draw_dsc(draw_task);
+    lv_draw_task_t * draw_task = lv_event_get_draw_task(e);
+    lv_draw_dsc_base_t * base_dsc = (lv_draw_dsc_base_t *)lv_draw_task_get_draw_dsc(draw_task);
 
-    const lv_chart_series_t *ser = lv_chart_get_series_next(obj, NULL);
+    const lv_chart_series_t * ser = lv_chart_get_series_next(obj, NULL);
     lv_color_t ser_color = lv_chart_get_series_color(obj, ser);
 
     /*Draw a triangle below the line witch some opacity gradient*/
-    lv_draw_line_dsc_t *draw_line_dsc = lv_draw_task_get_draw_dsc(draw_task);
+    lv_draw_line_dsc_t * draw_line_dsc = (lv_draw_line_dsc_t *)lv_draw_task_get_draw_dsc(draw_task);
     lv_draw_triangle_dsc_t tri_dsc;
 
     lv_draw_triangle_dsc_init(&tri_dsc);
@@ -69,17 +70,18 @@ static void add_faded_area(lv_event_t * e)
     tri_dsc.p[1].y = draw_line_dsc->p2.y;
     tri_dsc.p[2].x = draw_line_dsc->p1.y < draw_line_dsc->p2.y ? draw_line_dsc->p1.x : draw_line_dsc->p2.x;
     tri_dsc.p[2].y = LV_MAX(draw_line_dsc->p1.y, draw_line_dsc->p2.y);
-    tri_dsc.bg_grad.dir = LV_GRAD_DIR_VER;
+    tri_dsc.grad.dir = LV_GRAD_DIR_VER;
 
     int32_t full_h = lv_obj_get_height(obj);
     int32_t fract_uppter = (int32_t)(LV_MIN(draw_line_dsc->p1.y, draw_line_dsc->p2.y) - coords.y1) * 255 / full_h;
     int32_t fract_lower = (int32_t)(LV_MAX(draw_line_dsc->p1.y, draw_line_dsc->p2.y) - coords.y1) * 255 / full_h;
-    tri_dsc.bg_grad.stops[0].color = ser_color;
-    tri_dsc.bg_grad.stops[0].opa = 255 - fract_uppter;
-    tri_dsc.bg_grad.stops[0].frac = 0;
-    tri_dsc.bg_grad.stops[1].color = ser_color;
-    tri_dsc.bg_grad.stops[1].opa = 255 - fract_lower;
-    tri_dsc.bg_grad.stops[1].frac = 255;
+    tri_dsc.grad.stops[0].color = ser_color;
+    tri_dsc.grad.stops[0].opa = (lv_opa_t)(255 - fract_uppter);
+    tri_dsc.grad.stops[0].opa = 255 - fract_uppter;
+    tri_dsc.grad.stops[0].frac = 0;
+    tri_dsc.grad.stops[1].color = ser_color;
+    tri_dsc.grad.stops[1].opa = (lv_opa_t)(255 - fract_lower);
+    tri_dsc.grad.stops[1].frac = 255;
 
     lv_draw_triangle(base_dsc->layer, &tri_dsc);
 
@@ -89,7 +91,7 @@ static void add_faded_area(lv_event_t * e)
     rect_dsc.bg_grad.dir = LV_GRAD_DIR_VER;
     rect_dsc.bg_grad.stops[0].color = ser_color;
     rect_dsc.bg_grad.stops[0].frac = 0;
-    rect_dsc.bg_grad.stops[0].opa = 255 - fract_lower;
+    rect_dsc.bg_grad.stops[0].opa = (lv_opa_t)(255 - fract_lower);
     rect_dsc.bg_grad.stops[1].color = ser_color;
     rect_dsc.bg_grad.stops[1].frac = 255;
     rect_dsc.bg_grad.stops[1].opa = 0;
@@ -104,18 +106,19 @@ static void add_faded_area(lv_event_t * e)
 
 static void hook_division_lines(lv_event_t * e)
 {
-    lv_draw_task_t *draw_task = lv_event_get_draw_task(e);
-    lv_draw_dsc_base_t *base_dsc = lv_draw_task_get_draw_dsc(draw_task);
-    lv_draw_line_dsc_t *line_dsc = lv_draw_task_get_draw_dsc(draw_task);
+    lv_draw_task_t * draw_task = lv_event_get_draw_task(e);
+    lv_draw_dsc_base_t * base_dsc = (lv_draw_dsc_base_t *)lv_draw_task_get_draw_dsc(draw_task);
+    lv_draw_line_dsc_t * line_dsc = (lv_draw_line_dsc_t *)lv_draw_task_get_draw_dsc(draw_task);
 
     /*Vertical line*/
-    if (line_dsc->p1.x == line_dsc->p2.x) {
+    if(line_dsc->p1.x == line_dsc->p2.x) {
         line_dsc->color  = lv_palette_lighten(LV_PALETTE_GREY, 1);
-        if (base_dsc->id1 == 3) {
+        if(base_dsc->id1 == 3) {
             line_dsc->width = 2;
             line_dsc->dash_gap = 0;
             line_dsc->dash_width = 0;
-        } else {
+        }
+        else {
             line_dsc->width = 1;
             line_dsc->dash_gap = 6;
             line_dsc->dash_width = 6;
@@ -123,19 +126,21 @@ static void hook_division_lines(lv_event_t * e)
     }
     /*Horizontal line*/
     else {
-        if (base_dsc->id1 == 2) {
+        if(base_dsc->id1 == 2) {
             line_dsc->width  = 2;
             line_dsc->dash_gap  = 0;
             line_dsc->dash_width  = 0;
-        } else {
+        }
+        else {
             line_dsc->width = 2;
             line_dsc->dash_gap  = 6;
             line_dsc->dash_width  = 6;
         }
 
-        if (base_dsc->id1 == 1  || base_dsc->id1 == 3) {
+        if(base_dsc->id1 == 1  || base_dsc->id1 == 3) {
             line_dsc->color  = lv_palette_main(LV_PALETTE_GREEN);
-        } else {
+        }
+        else {
             line_dsc->color  = lv_palette_lighten(LV_PALETTE_GREY, 1);
         }
     }
